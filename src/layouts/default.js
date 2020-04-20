c79bb06a306ffd904301991f58a762eae53e21c9
LucasCtrl <lucasalt@protonmail.com>
2020-04-20 16:00:03 +0200

    Fix the footer at the bottom of the page

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -5,9 +5,11 @@ import Footer from "../components/footer"
 import "../styles/main.scss"
 
 export default ({ children }) => (
-  <div className="container">
-    <Nav />
-    {children}
+  <div className="main">
+    <div className="container">
+      <Nav />
+      {children}
+    </div>
     <Footer />
   </div>
 )
