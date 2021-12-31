8554c36908cf70db0af0670be54f1e9dcb87f435
LucasCtrl <lucasalt@protonmail.com>
2021-12-31 16:32:44 +0100

    🔍 Improve SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,53 +1,63 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
   generate: {
     fallback: true,
   },
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'LucasAlt',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
-    ],
+
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['./assets/css/main.css'],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxt/postcss8',
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-  ],
+  buildModules: ['@nuxt/postcss8', '@nuxtjs/eslint-module'],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-  ],
+  modules: ['@nuxtjs/pwa'],
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
+    meta: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+
+      name: '',
+      author: 'LucasAlt',
+      description:
+        'Web developer, UI/UX designer, minimalist, i leave my mark on this immense web that is the internet.',
+      theme_color: '#1d4ed8',
+      lang: 'en',
+
+      // Open Graph
+      ogType: 'website',
+      // ogSiteName: '' // Retreive 'name' by default
+      // ogTitle: '' // Retreive 'name' by default
+      // ogDescription: '' // Retreive 'description' by default
+      ogHost: 'https://lucasalt.fr', // Domain where the icon is hosted
+      ogImage: true, // Use app icon
+      // ogUrl // Same as 'ogHost'
+
+      // Twitter
+      twitterCard: 'summary', // Twitter card type
+      twitterSite: '@LucasCtrlAlt', // Twitter owner handle
+      twitterCreator: '@LucasCtrlAlt', // Twitter developer handle
+    },
     manifest: {
+      name: 'LucasAlt | Personal website',
+      short_name: 'LucasAlt',
+      description:
+        'Web developer, UI/UX designer, minimalist, i leave my mark on this immense web that is the internet.',
+      background_color: '#f9fafb',
+      dir: 'ltr',
       lang: 'en',
     },
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     postcss: {
       plugins: {
