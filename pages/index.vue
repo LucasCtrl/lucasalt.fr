8c5792efa9f499c6164523de21de8af9f62a4c3e
LucasCtrl <lucasalt@protonmail.com>
2021-12-31 14:56:12 +0100

    ✨ About section on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,62 @@
 <template>
-  <div class="mt-[20%]">
-    <h1 class="text-5xl font-semibold">
-      Hey, I'm <br />
-      <span class="text-blue-700 dark:text-blue-600 font-bold">Lucas ALBERT</span>
-    </h1>
-    <h1 class="pt-4 text-2xl text-gray-600 dark:text-gray-500 font-normal">
-      Web developer / Maker / Love to play video games
-    </h1>
+  <div>
+    <div class="mt-[15%]">
+      <h1 class="text-5xl font-semibold">
+        Hey, I'm <br />
+        <span class="text-blue-700 dark:text-blue-600 font-bold"
+          >Lucas ALBERT</span
+        >
+      </h1>
+      <h1 class="pt-4 text-2xl text-gray-600 dark:text-gray-500">
+        Web developer / Maker / Love to play video games
+      </h1>
+    </div>
+    <div class="mt-[15%] text-xl space-y-4 text-justify">
+      <p>
+        22 years old French web developer with more than five years of
+        experience, I develop web applications and I am specialised in the
+        development of ergonomic and minimalist interfaces.
+      </p>
+      <p>
+        Currently instructor on
+        <a
+          href="https://en.wikipedia.org/wiki/Computer-aided_design"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="text-pink-500 hover:underline"
+          >CAD software</a
+        >
+        and
+        <a
+          href="https://en.wikipedia.org/wiki/Numerical_control"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="text-blue-500 hover:underline"
+          >CNC machines</a
+        >, I take advantage of my free time to do prototyping in the mechanical
+        keyboard area.
+      </p>
+      <p>
+        I'm also working on
+        <a
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="text-green-500 hover:underline"
+          >CanvasMe</a
+        >
+        library. A library for creating canvas in a simplified way. In parallel
+        I maintain
+        <a
+          href="https://github.com/lucasctrl/spookybot"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="text-orange-500 hover:underline"
+          >SpookyBot</a
+        >, a little terror that comes to scare you on your Discord server.
+        Project initiated during the Hacktoberfest 2020.
+      </p>
+    </div>
   </div>
 </template>
 
