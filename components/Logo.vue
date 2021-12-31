20bdf5c6a776d6978dba41a6d1a11962449e891a
LucasCtrl <lucasalt@protonmail.com>
2021-12-31 15:32:27 +0100

    💄 Update current logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,10 +1,11 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="currentColor" viewBox="0 0 100 100">
-    <g>
-      <path d="M20,100l0,-100l-20,0l0,100l20,0Z" />
-      <path d="M0,80l45.8,0l16,20l-61.8,0l0,-20Z" />
-      <path d="M100,100l-25.612,0l-70.005,-87.506l15.617,-12.494l80,100Z" />
-    </g>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-12 w-12"
+    fill="currentColor"
+    viewBox="0 0 1024 1024"
+  >
+    <path d="M200 0L1024 1024H767L200 318V824H479.5L640 1024H0V0H200Z" />
   </svg>
 </template>
 
