353f548df7eb4f0a65bb915e5fa62100d660945c
LucasCtrl <lucasalt@protonmail.com>
2022-01-02 18:02:53 +0100

    ✨ Add blockquote component

---
+++ components/global/BlockquoteComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <blockquote :class="`rounded ${blockquoteClasses}`">
+    <div>
+      <p class="font-bold"><slot name="title"></slot></p>
+      <p><slot name="content"></slot></p>
+    </div>
+    <p class="mt-2 text-sm text-slate-700 dark:text-slate-300">
+      <slot name="source"></slot>
+    </p>
+  </blockquote>
+</template>
+
+<script>
+export default {
+  name: 'BlockquoteComponent',
+  props: {
+    color: {
+      type: String,
+      default: '',
+    },
+  },
+
+  data: () => ({
+    blockquoteClasses: '',
+  }),
+
+  mounted() {
+    if (this.color === 'warning')
+      this.blockquoteClasses = 'bg-amber-400/20 border-l-4 border-amber-400'
+    else this.blockquoteClasses = 'bg-slate-400/20 border-l-4 border-slate-400'
+  },
+}
+</script>
