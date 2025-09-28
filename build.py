173c04a1f8cf20218a3deb2cdd5dda6a53d66582
LucasCtrl <lucasalt@protonmail.com>
2025-09-28 10:24:35 +0200

    Copy static files and generate posts

--- build.py
+++ build.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 from liquid import Environment
 from liquid import FileSystemLoader
 from pathlib import Path
@@ -23,5 +25,32 @@ def listPages():
 
   return pageList
 
-# print(listPages())
+def copyStaticContent():
+  if not os.path.exists('dist/styles'):
+      os.makedirs('dist/styles')
+
+  shutil.rmtree('dist/styles')
+  shutil.copytree('src/styles', 'dist/styles')
+
+def createPostPages():
+  env=Environment(loader=FileSystemLoader("src/templates", ext=".html"))
+  template = env.get_template('default')
+
+  posts = Path('src/posts').glob('**/*.html')
+
+  for post in posts:
+    if not os.path.exists('dist/posts'):
+      os.makedirs(f'dist/posts/{post.stem}')
+
+    postContent = open(post, 'r').read()
+    with open(f'dist/posts/{post.stem}/index.html', 'w') as file:
+      file.write(template.render(page_content=postContent, links=listPages()))
+
+  # for page in pages:
+  #   pageContent = open(page, 'r').read()
+  #   with open(f'dist/{page.name}', 'w') as file:
+  #     file.write(template.render(page_content=pageContent, links=listPages()))
+
+copyStaticContent()
 createPages()
+createPostPages()
