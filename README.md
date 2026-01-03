bee583b2b36015a9267e620df4c6491654797d50
LucasCtrl <lucasalt@protonmail.com>
2026-01-03 21:08:13 +0100

    Display tags on post pages

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ pip install watchdog
 
 # Run the watcher to rebuild everytime a change is done
 python -X dev watcher.py
+
+# Start a live server on 127.0.0.1:8000 or 0.0.0.0:8000 - https://docs.python.org/3/library/http.server.html#command-line-interface
+python -m http.server 8000 --directory dist/
 ```
--- build.py
+++ build.py
@@ -1,12 +1,13 @@
 import os
-import sys
 import shutil
-import frontmatter
+import sys
 from pathlib import Path
+
+import frontmatter
+from liquid import Environment, FileSystemLoader
 from marko import Markdown
 from marko.html_renderer import HTMLRenderer
-from liquid import Environment
-from liquid import FileSystemLoader
+
 
 class CustomMarkoRenderer(HTMLRenderer):
   def render_image(self, element):
@@ -22,10 +23,13 @@ class CustomMarkoRenderer(HTMLRenderer):
 
     return html
 
+
 markdown = Markdown(extensions=['gfm', 'footnote'], renderer=CustomMarkoRenderer)
+
 
 def log(message):
   print(f'[Log] - {message}')
+
 
 # Get all posts listed in 'posts' folder
 def getPosts():
@@ -35,11 +39,30 @@ def getPosts():
     with open(postFile) as f:
       metadata, content = frontmatter.parse(f.read())
       if sys.flags.dev_mode:
-        postList.append({'title': metadata['title'], 'publishDate': metadata['publishDate'], 'uri': f'posts/{postFile.stem}', 'content': content})
+        postList.append(
+          {
+            'title': metadata['title'],
+            'publishDate': metadata['publishDate'],
+            'tags': metadata['tags'] if 'tags' in metadata else [],
+            'uri': f'posts/{postFile.stem}',
+            'content': content,
+          }
+        )
       else:
         if metadata['published']:
-          postList.append({'title': metadata['title'], 'publishDate': metadata['publishDate'], 'uri': f'posts/{postFile.stem}', 'content': content})
+          postList.append(
+            {
+              'title': metadata['title'],
+              'publishDate': metadata['publishDate'],
+              'tags': metadata['tags'] if 'tags' in metadata else [],
+              'uri': f'posts/{postFile.stem}',
+              'content': content,
+            }
+          )
+
+  # print(postList)
   return postList
+
 
 # Get all pages listed in 'pages' folder
 def getPages():
@@ -49,29 +72,44 @@ def getPages():
     with open(pageFile) as f:
       metadata, content = frontmatter.parse(f.read())
       if sys.flags.dev_mode:
-        pageList.append({'title': metadata['title'], 'uri': pageFile.stem, 'content': content})
+        pageList.append(
+          {
+            'title': metadata['title'],
+            'uri': pageFile.stem,
+            'content': content,
+          }
+        )
       else:
         if metadata['published']:
-          pageList.append({'title': metadata['title'], 'uri': pageFile.stem, 'content': content})
+          pageList.append(
+            {
+              'title': metadata['title'],
+              'uri': pageFile.stem,
+              'content': content,
+            }
+          )
   return pageList
+
 
 # Generate static content
 def copyStaticContent(outputFolder):
   log('Copying static files')
   shutil.copytree('src/static', outputFolder, dirs_exist_ok=True)
 
+
 # Generate index page
 def createIndexPage(outputFolder):
-  env=Environment(loader=FileSystemLoader("src/templates", ext=".html"))
+  env = Environment(loader=FileSystemLoader('src/templates', ext='.html'))
   template = env.get_template('index')
 
   log('Creating index.html')
   with open(f'{outputFolder}/index.html', 'w') as file:
     file.write(template.render(postList=getPosts()))
 
+
 # Generate pages
 def createPages(outputFolder):
-  env=Environment(loader=FileSystemLoader("src/templates", ext=".html"))
+  env = Environment(loader=FileSystemLoader('src/templates', ext='.html'))
   template = env.get_template('page')
 
   for page in getPages():
@@ -86,15 +124,17 @@ def createPages(outputFolder):
     with open(f'{outputFolder}/{pageUri}/index.html', 'w') as file:
       file.write(template.render(pageContent=pageContent, title=pageTitle))
 
+
 # Generate posts pages
 def createPostsPage(outputFolder):
-  env=Environment(loader=FileSystemLoader("src/templates", ext=".html"))
+  env = Environment(loader=FileSystemLoader('src/templates', ext='.html'))
   template = env.get_template('post')
 
   for post in getPosts():
     postUri = post['uri']
     postTitle = post['title']
     postPublishDate = post['publishDate']
+    postTags = post['tags']
     postContent = markdown.convert(post['content'])
 
     if not os.path.exists(f'dist/{postUri}'):
@@ -102,7 +142,15 @@ def createPostsPage(outputFolder):
 
     log(f'Creating {postUri}/index.html')
     with open(f'{outputFolder}/{postUri}/index.html', 'w') as file:
-      file.write(template.render(postContent=postContent, title=postTitle, publishDate=postPublishDate))
+      file.write(
+        template.render(
+          postContent=postContent,
+          title=postTitle,
+          publishDate=postPublishDate,
+          tags=postTags,
+        )
+      )
+
 
 def main():
   outputFolder = 'dist'
@@ -119,6 +167,7 @@ def main():
   createIndexPage(outputFolder)
   createPostsPage(outputFolder)
   createPages(outputFolder)
+
 
 if __name__ == '__main__':
   main()
